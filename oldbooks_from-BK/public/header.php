660658744b43f8d96a67ff70f2607b77295d0b06
albertdeng <albertdeng@kkbox.com>
2017-08-22 01:33:06 +0800

    old files

---
+++ oldbooks_from-BK/public/header.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=Edge;chrome=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<meta name="keywords" content="">
+<meta name="description" content="">
+<title>國立臺灣歷史博物館 古書契資料庫</title>
+
+
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/magnific-popup.css">
+<link href="css/jquery.bxslider.css" rel="stylesheet" />
+<link rel="stylesheet" href="css/module.css">
+<link rel="stylesheet" href="css/modify.css">
+
+<script src="js/jquery-1.11.1.min.js"></script>
+
+<script type="text/javascript" src="http://momentjs.com/downloads/moment.min.js"></script>
+<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+
+<script src="js/jquery.bxslider.min.js"></script>
+<script src="js/ui.js"></script>
+
+<script>
+	$(function(){
+		$('.open-popup-link').magnificPopup({
+		  type:'inline',
+		  midClick: true 
+		  // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+		});
+	});
+</script>
+
+
+</head>
+<body>
+<div class="top_header">
+	<div class="top_header_left">
+		<img src="img/museumLogo.png" >
+	</div>
+	<div class="top_header_right">
+		<a href="">關於本站</a>   |  <a href="">資料說明 </a>  |  <a href="">操作說明 </a> |  <a href="">常見問題</a>  |  <a href="">聯絡我們</a> |  <a href="">問題回報</a> |  <a href="">會員專區</a>  |  <a href=""><i class="fa fa-user fa-2x" aria-hidden="true"></i></a>
+	</div>
+</div>
+
+<header>
+	<div class="logo">
+		<img src="img/logo.png" alt="">
+	</div>	
+	
+	<div class="search_section">
+		<div class="search_item">
+			<div class="search_go">
+				<input type="text">
+				<div class="serach_icon"><i class="fa fa-search" aria-hidden="true"></i></div>
+				<div class="serach_icon_menu"><i class="fa fa-caret-down fa-2x" aria-hidden="true"></i></div>
+			</div>
+			<div class="search_go_menu">
+					<div class="menu_bar">
+						<div class="menu_bar_left" style="width:20%">起始日期 ：</div>
+						<div class="menu_bar_center" style="width:20%"><input type="text"></div>
+						<div class="menu_bar_left" style="width:20%">結束日期 ：</div>
+						<div class="menu_bar_center" style="width:20%"><input type="text"></div>
+						<div class="menu_bar_right"></div>
+					</div>
+					<div class="menu_bar">
+						<div class="menu_bar_left">題名 ：</div>
+						<div class="menu_bar_center"><input type="text"></div>
+						<div class="menu_bar_right"></div>
+					</div>
+					<div class="menu_bar">
+						<div class="menu_bar_left">立契人 ：</div>
+					<div class="menu_bar_center"><input type="text"></div>
+						<div class="menu_bar_right"></div>
+					</div>
+					<div class="menu_bar">
+						<div class="menu_bar_left">描述 ：</div>
+						<div class="menu_bar_center"><input type="text"></div>
+						<div class="menu_bar_right"></div>
+					</div>
+					<div class="menu_bar">
+						<div class="menu_bar_left">全宗名 ：</div>
+						<div class="menu_bar_center"><input type="text"></div>
+						<div class="menu_bar_right"></div>
+					</div>
+					<div class="menu_bar">
+						<div class="menu_bar_left">古地名 ：</div>
+						<div class="menu_bar_center"><input type="text"></div>
+						<div class="menu_bar_right"></div>
+					</div>
+					<div class="menu_bar">
+						<div class="menu_bar_left">今地名 ：</div>
+						<div class="menu_bar_center"><input type="text"></div>
+						<div class="menu_bar_right"></div>
+					</div>	
+				<div class="menu_bar">
+						<div class="menu_bar_left"></div>
+						<div class="menu_bar_center"></div>
+						<div class="menu_bar_right"><div class="serach_icon_menu"><i class="fa fa-caret-down fa-2x" aria-hidden="true"></i></div></div>
+					</div>							
+			</div>
+
+			
+		</div>
+	</div>
+
+		
+
+	<!-- mobile menu -->
+	<div class="hamburg">
+		<span class="aaa"></span>
+		<span class="bbb"></span>
+		<span class="ccc"></span>
+	</div>
+	<div class="aside">
+		<ul>
+			<li><a href="#" class="m-first">分類瀏覽</a></li>
+			<li><a href="#" class="m-first">縣市記憶</a></li>
+			<li><a href="#" class="m-first">記憶窗口</a></li>
+			<li><a href="#" class="m-first">線上特展</a></li>
+			<li><a href="#" class="m-first">主題瀏覽</a></li>
+			<li><a href="#" class="m-first">數位授權</a></li>
+
+		</ul>
+	</div>
+
+
+
+</header>
